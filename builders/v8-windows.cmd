71e0b6c89eeec49dfffb5e0470314bb97fe78efe
奶嘴 <chenxi2511@qq.com>
2024-07-01 15:31:26 +0800

    Update v8-windows.cmd

--- builders/v8-windows.cmd
+++ builders/v8-windows.cmd
@@ -24,13 +24,13 @@ echo target_os = ['win'] >> .gclient
 cd v8
 call git checkout %VERSION%
 call gclient sync
-call git apply --ignore-whitespace --verbose %GITHUB_WORKSPACE%\builders\BUILD.gn.patch
+:: call git apply --ignore-whitespace --verbose %GITHUB_WORKSPACE%\builders\BUILD.gn.patch
 
 
-echo =====[ Building V8 ]=====
+:: echo =====[ Building V8 ]=====
 :: call python .\tools\dev\v8gen.py x64.release -vv -- target_os="""win""" is_component_build=true use_custom_libcxx=false is_clang=true use_lld=false v8_enable_verify_heap=false v8_enable_i18n_support=true v8_use_external_startup_data=false symbol_level=0
 
 :: call ninja -C out.gn\x64.release -t clean
 :: call ninja -C out.gn\x64.release v8
-cd ..
-7z a v8.zip v8\
+:: cd ..
+:: 7z a v8.zip v8\
